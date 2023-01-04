c756fc244eda3566969a8eee22833029a482603b
Brendan Baird <brendan.b517@gmail.com>
2023-01-04 16:59:38 -0500

    Started the fetch request

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 fetch("http://localhost:3000")
 .then(response => response.json())
 .then(guitars => {
-    
+    const acousticGuitars = guitars.acoustic;
+    const electricGuitars = guitars.electric;
+    const inventoryColumn = document.getElementById("inventory-column");
+    const mainBody = document.getElementById("main-body");
+
+    function displayGuitarDetails(guitar){
+        const newGuitarImage = document.getElementById("image");
+        newGuitarImage.src = guitar.image;
+    }
+    function populateInventory(inventory){
+        inventoryColumn.innerHTML = "";
+        inventory.forEach((guitar) => {
+            const newGuitarListElement = document.createElement("li");
+            const newGuitar = document.createElement("img");
+            newGuitar.src = guitar.image;
+            newGuitar.addEventListener("click", (event) => {
+                event.preventDefault();
+            })
+            newGuitarListElement.appendChild(newGuitar);
+            inventoryColumn.appendChild(newGuitarListElement);
+        })
+    }
 })
